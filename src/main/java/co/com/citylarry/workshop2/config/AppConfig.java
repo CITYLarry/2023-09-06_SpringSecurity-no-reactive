194924e5773edd4ee19cf71dde38358b77115994
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-09-06 14:51:51 -0500

    Initial commit

---
+++ src/main/java/co/com/citylarry/workshop2/config/AppConfig.java
@@ -0,0 +1,44 @@
+package co.com.citylarry.workshop2.config;
+
+import co.com.citylarry.workshop2.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository repository) {
+        return username -> repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(
+            UserRepository repository,
+            PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService(repository));
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
---
+++ src/main/java/co/com/citylarry/workshop2/user/Role.java
@@ -0,0 +1,6 @@
+package co.com.citylarry.workshop2.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
